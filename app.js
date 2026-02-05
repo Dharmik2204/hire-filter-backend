e3a67440c58bd99211ccf450b593582ac500b346
Dharmik2204 <dharmikkhalas22@gmail.com>
2026-02-05 12:53:00 +0530

    Disable Exam and Rank API as requested to restore deployment stability

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import authRoutes from "./src/routes/auth.routes.js";
 import jobRoutes from "./src/routes/job.routes.js";
 import application from "./src/routes/application.routes.js";
 import userRoute from "./src/routes/users.routes.js";
-import examRoutes from "./src/routes/exam.routes.js";
-import rankRoutes from "./src/routes/rank.routes.js";
+// import examRoutes from "./src/routes/exam.routes.js";
+// import rankRoutes from "./src/routes/rank.routes.js";
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoute);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/application", application);
-app.use("/api/exams", examRoutes);
-app.use("/api/ranks", rankRoutes);
+// app.use("/api/exams", examRoutes);
+// app.use("/api/ranks", rankRoutes);
 
 export default app;
