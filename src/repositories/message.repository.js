82b762a2b9c0710aaff4a10452cf8715fe0afda8
Dharmik2204 <dharmikkhalas22@gmail.com>
2026-02-14 13:33:39 +0530

    update message id problme

--- src/repositories/message.repository.js
+++ src/repositories/message.repository.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Message } from "../models/message.models.js";
 
 export const createMessage = (data) => {
@@ -9,10 +10,13 @@ export const findMessageById = (id) => {
 };
 
 export const getConversationHistory = (userId1, userId2) => {
+    const id1 = new mongoose.Types.ObjectId(userId1);
+    const id2 = new mongoose.Types.ObjectId(userId2);
+
     return Message.find({
         $or: [
-            { sender: userId1, receiver: userId2 },
-            { sender: userId2, receiver: userId1 }
+            { sender: id1, receiver: id2 },
+            { sender: id2, receiver: id1 }
         ]
     }).sort({ createdAt: 1 }); // Oldest first
 };
