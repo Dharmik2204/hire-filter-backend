d764239ed58f18ba68f042c20c6206ed2e24a1bb
Dharmik2204 <dharmikkhalas22@gmail.com>
2026-02-24 12:18:33 +0530

    update route

--- src/routes/exam.routes.js
+++ src/routes/exam.routes.js
@@ -17,33 +17,32 @@ import { validateObjectIdParams } from "../middlewares/validateObjectId.middlewa
 
 const router = express.Router();
 
-//this protects every route in this file in just one line
-router.use(validateObjectIdParams);
+
 
 /* =====================
    HR ROUTES
 ===================== */
 router.post("/", authMiddleware, authorizeRoles("hr", "admin"), createExamController);
-router.get("/job/:jobId", authMiddleware, getExamByJobController);
-router.get("/:examId/attempts", authMiddleware, authorizeRoles("hr", "admin"), getExamAttemptsController);
-router.get("/attempt/:attemptId", authMiddleware, authorizeRoles("hr", "admin"), getAttemptDetailsController);
-router.delete("/:examId", authMiddleware, authorizeRoles("hr", "admin"), deleteExamController);
+router.get("/job/:jobId", validateObjectIdParams, authMiddleware, getExamByJobController);
+router.get("/:examId/attempts", validateObjectIdParams, authMiddleware, authorizeRoles("hr", "admin"), getExamAttemptsController);
+router.get("/attempt/:attemptId", validateObjectIdParams, authMiddleware, authorizeRoles("hr", "admin"), getAttemptDetailsController);
+router.delete("/:examId", validateObjectIdParams, authMiddleware, authorizeRoles("hr", "admin"), deleteExamController);
 
 /* =====================
    USER ROUTES
 ===================== */
-router.post("/start", authMiddleware, startExamController);
-router.post("/:attemptId/submit", authMiddleware, submitExamController);
+router.post("/start", validateObjectIdParams, authMiddleware, startExamController);
+router.post("/:attemptId/submit", validateObjectIdParams, authMiddleware, submitExamController);
 
 /* =====================
    SYSTEM / ADMIN
 ===================== */
 // router.post("/:attemptId/evaluate", authMiddleware, authorizeRoles("admin", "hr"), evaluateExamController);
-router.patch("/:attemptId/feedback", authMiddleware, authorizeRoles("admin", "hr"), addExamFeedbackController);
+router.patch("/:attemptId/feedback", validateObjectIdParams, authMiddleware, authorizeRoles("admin", "hr"), addExamFeedbackController);
 
 /* =====================
    USER REPORT
 ===================== */
-router.get("/my-result/:examId", authMiddleware, authorizeRoles("user"), getMyExamResult);
+router.get("/my-result/:examId", validateObjectIdParams, authMiddleware, authorizeRoles("user"), getMyExamResult);
 
 export default router;
